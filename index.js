be1076f203395aec7c73d1174b5801e40aa3b499
ellamsg <ellamsgeorge7@gmail.com>
2022-08-26 15:00:08 -0700

    icons

--- index.js
+++ index.js
@@ -26,4 +26,15 @@ function navLinkClick() {
     navToggler.click();
   }
 }
-  
+  //count animation
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
