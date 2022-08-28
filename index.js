0c2627dd3446e65947800d4c22defc7d64b7bd91
ellamsg <ellamsgeorge7@gmail.com>
2022-08-28 03:58:41 -0700

    dance icons update

--- index.js
+++ index.js
@@ -26,15 +26,52 @@ function navLinkClick() {
     navToggler.click();
   }
 }
-  //count animation
-  $('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
+
+
+/*
+jQuery(document).ready(function() {
+  var imgs = jQuery('img-expand').children();
+  var contWidth = jQuery('#img-container').width();
+  
+  
+  var bigWidth = 30;
+  var smallWidth = (100 - bigWidth) / (imgs.length);
+  var mouseSmalLWidth = (100/imgs.length - 1);
+  var normalWidth = (100/ imgs.length);
+  // var smallWidth =  (contWidth - bigWidth) / (imgs.length - 1);
+  
+  console.log(smallWidth);
+  console.log(bigWidth);
+  // console.log(imgs.length);
+  // console.log(contWidth);
+  
+  jQuery('.img-expand').mouseover(function() {
+    jQuery(this).stop().animate({
+      // width: bigWidth+'%'
+      width: '40%'
+    }, 300);
+    
+    jQuery(this).siblings().stop().animate( {
+      // width: smallWidth+'%'
+      width: '15%'
+    }, 300);
+    
+  })
+  
+  jQuery('.img-expand').mouseout(function(){
+    jQuery(this).stop().animate({
+      // width: smallWidth+'%'
+      width: '20%'
+    }, 300);
+    jQuery(this).siblings().stop().animate( {
+      // width: smallWidth+'%'
+      width: '20%'
+    }, 300);
+  })
+  
+  
 });
+*/
+
+
+//testing
